bfd0ee3d320a1b0b06edc16eb5cf985cb48a922f
Andrew MacIsaac <amacisaac@sierrawireless.com>
2021-11-23 14:04:44 -0800

    [LE-15808] WIP on GDB protocol state machine
    Ensure some unfinished work is not lost.  No guarantees are made
    regarding the state of the code at this point.
    
    Resolves: LE-15808
    Change-Id: Ie7a689c7d7e1523b0fb594ccc710d0d80423fbd0

---
+++ source/protocol/ack.c
@@ -0,0 +1,70 @@
+/**
+ *  @file       ack.c
+ *  @copyright  2021 Sierra Wireless Inc.
+ *
+ *  @remark
+ *      SPDX-License-Identifier: MPL-2.0
+ *
+ *  @brief      ACK handling for the GDB protocol.
+ */
+#include "ack.h"
+
+#include "core.h"
+#include "stdc/assert.h"
+#include "stdc/null.h"
+
+/**
+ * Control whether ACK and NACK packets are sent and received after regular data packets.
+ */
+void proto_set_ack_mode
+(
+    int enable ///< Boolean value to enable or disable ACK support.
+)
+{
+    core_get_environment()->ack_enabled = enabled;
+}
+
+/**
+ * Prepare and send the requested ack packet.  If ack support is disabled, then no action is taken.
+ */
+static void send
+(
+    const char *type ///< Type of ack message, either "ACK" or "NACK" is expected.
+)
+{
+    int                     result;
+    struct packet_writer    packet;
+
+    assert(type != NULL);
+
+    if (core_get_environment()->ack_enabled)
+    {
+        packet_writer_init(&packet, PT_ACK, GDBS_PACKET_SINK, GDBS_PACKET_SINK_PARAM);
+
+        packet_writer_push_ack((type[0] == 'A'), &packet)
+        if (result == 0)
+        {
+            result = packet_writer_finish(&packet);
+        }
+        if (result < 0)
+        {
+            GDBS_LOG("Failed to send %s: %s\n", type, gdbs_error_to_string(-result));
+        }
+    }
+}
+
+/**
+ * Send an ACK response, if ack support is enabled.
+ */
+void proto_ack(void)
+{
+    send("ACK");
+}
+
+/**
+ * Send an NACK response, if ack support is enabled.
+ */
+void proto_nack(void)
+{
+    send("NACK");
+}
---
+++ source/protocol/receive.c
@@ -0,0 +1,125 @@
+/**
+ *  @file       receive.c
+ *  @copyright  2021 Sierra Wireless Inc.
+ *
+ *  @remark
+ *      SPDX-License-Identifier: MPL-2.0
+ *
+ *  @brief      Packet reception implementation for GDB protocol.
+ */
+#include "gdbsconfig.h"
+
+#include "receive.h"
+
+#include "core.h"
+
+// void proto_receive_packet
+// (
+//     struct packet_tokenizer *tokenizer
+// )
+// {
+//     const char      *token = NULL;
+//     int              result = 0;
+//     unsigned long    length = 0;
+
+//     // Extract the first token of the packet, which indicates the type of command.
+//     result = packet_tokenizer_advance(tokenizer, TOKEN_SINGLE_CHAR, &token, &length);
+//     if (result < 0)
+//     {
+//         GDBS_LOG("Packet tokenization failed: %s", gdbs_error_to_string(-result));
+//         return 0;
+//     }
+//     else if (length == 0)
+//     {
+//         GDBS_LOG("Ignoring zero-length packet token");
+//         return 0;
+//     }
+
+//     // Dispatch packet to appropriate handler based on initial character.
+//     switch (token[0])
+//     {
+//         case '?':
+//             result = proto_stop_reply();
+//             break;
+//         case 'c':
+//             result = proto_continue(tokenizer);
+//             break;
+//         case 'g':
+//             result = proto_read_general_registers(tokenizer);
+//             break;
+//         case 'G':
+//             result = proto_write_general_registers(tokenizer);
+//             break;
+//         case 'k':
+//             result = proto_kill();
+//             break;
+//         case 'm':
+//             result = proto_read_memory(tokenizer);
+//             break;
+//         case 'M':
+//             result = proto_write_memory(tokenizer);
+//             break;
+//         case 'q':
+//             result = proto_general_query(tokenizer);
+//             break;
+//         case 's':
+//             result = proto_single_step(tokenizer);
+//             break;
+//         case 'v':
+//             result = proto_extended_command(tokenizer);
+//             break;
+//         default:
+//             GDBS_LOG("Unrecognized command prefix: '%c'\n", token[0]);
+//             break;
+//     }
+
+//     return result;
+// }
+
+
+/**
+ * Enter the packet receive and process loop.
+ */
+void proto_process
+(
+    int send_stop_reply ///< Boolean indicating to send an initial stop reply with information about
+                        ///< the signal that triggered entry into the debugger.
+)
+{
+    int                      result;
+    struct environment      *env = core_get_environment();
+    struct packet_tokenizer  tokenizer;
+    unsigned long            length;
+
+    if (send_stop_reply)
+    {
+        // Send information regarding the signal that caused control to pass the stub.
+        proto_send_stop_reply();
+    }
+
+    for (;;)
+    {
+        // Wait for the next packet to arrive.
+        length = GDBS_PACKET_BUFFER_LENGTH;
+        result = packet_receive(buffer, &length, PT_MESSAGE, env.comm);
+        if (result == 0)
+        {
+            proto_send_ack(1);
+        }
+        {
+            GDBS_LOG("Receive error: %s", gdbs_error_to_string(-result));
+            proto_send_ack(0);
+            continue;
+        }
+
+        // Begin parsing the packet.
+        packet_tokenizer_init(&tokenizer, buffer, length);
+
+        // Try to dispatch the packet to a handler.
+        if (proto_receive_packet(&tokenizer))
+        {
+            // Exit from the processing handler, if requested.
+            break;
+        }
+    }
+}
